dccb96b72f14d05ecd6395cd594b63440c64a09d
RobertoGrijalvaSanchez <roberd.999117@gmail.com>
2024-05-05 13:36:18 -0600

    commit by Roberto Grijalva Sanchez

---
+++ #00 SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/Program.cs
@@ -0,0 +1,18 @@
+﻿//https://dotnet.microsoft.com/es-es/languages/csharp
+
+//Comentario de una sola línea
+
+/*
+Comentarios en  
+distintas líneas
+*/
+
+int variable1 = 0;
+double variable2 = 1.0;
+String variable3 = "C#";
+bool variable4 = false;
+char variable5 = 'r';
+
+const int variable6 = 1;
+
+Console.WriteLine("¡Hola, "+variable3+"!");
