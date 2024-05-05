471817ae2e4aa8a30ed8ca62d2cec050913d96f2
RobertoGrijalvaSanchez <roberd.999117@gmail.com>
2024-05-05 15:01:16 -0600

    commit by Roberto Grijalva Sanchez

--- #02 FUNCIONES Y ALCANCE/Program.cs
+++ #02 FUNCIONES Y ALCANCE/Program.cs
@@ -1,2 +1,74 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿saludo();
+int a = 10, b = 5;
+mayor(b,a);
+Console.WriteLine("La suma de los numeros es: "+suma(a,b));
+Console.WriteLine("La cantidad de ocurrencias es: "+multiplos("str1", "str2"));
+void saludo()
+{
+    Console.WriteLine("Este es un saludo genérico.");
+}
+
+void mayor(int numero1, int numero2)
+{
+    if (numero1 > numero2)
+    {
+        Console.WriteLine("El numero 1 -> "+numero1+ ", es el mayor!");
+    } else if (numero1 < numero2)
+    {
+        Console.WriteLine("El numero 2 -> "+numero2+", es el mayor!");
+    } else
+    {
+        Console.WriteLine("Ninguno es mayor que el otro");
+    }
+}
+
+int suma(int numero1, int numero2)
+{
+    return numero1 + numero2;
+}
+
+void mayor2(int numero1, int numero2)
+{
+    void mensajeMayor(int n)
+    {
+        Console.WriteLine("El numero: " + n + ", es el mayor!");
+    }
+
+    if (numero1 > numero2)
+    {
+        mensajeMayor(numero1);
+        
+    }
+    else if (numero1 < numero2)
+    {
+        mensajeMayor(numero2);
+    }
+    else
+    {
+        Console.WriteLine("Ninguno es mayor que el otro");
+    }
+}
+
+int multiplos(String str1, String str2)
+{
+    int contador = 0;
+    for (int i = 0; i < 101;i++)
+    {
+        if (i % 3 == 0 && i % 5 == 0)
+        {
+            Console.WriteLine(str1);
+            contador++;
+        }else if (i % 5 == 0)
+        {
+            Console.WriteLine(str2);
+            contador++;
+        }
+        else if (i % 3 == 0)
+        {
+            Console.WriteLine(str1+str2);
+            contador++;
+        }
+    }
+    return contador;
+}
+//-Roberto Grijalva Sanchez
--- #02 FUNCIONES Y ALCANCE/obj/Debug/net8.0/#02 FUNCIONES Y ALCANCE.AssemblyInfo.cs
+++ #02 FUNCIONES Y ALCANCE/obj/Debug/net8.0/#02 FUNCIONES Y ALCANCE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("#02 FUNCIONES Y ALCANCE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9434dd4e14abb84771b4df03a01f523b78870a62")]
 [assembly: System.Reflection.AssemblyProductAttribute("#02 FUNCIONES Y ALCANCE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("#02 FUNCIONES Y ALCANCE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
